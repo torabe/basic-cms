80a4d265265947430745da8f4067ef81557dfe91
torabe <s2808282245@gmail.com>
2021-03-14 19:13:22 +0900

    prettier + package.json + webpack.mix設定

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,7 @@
 const mix = require('laravel-mix');
 
+const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin');
+
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -11,5 +13,35 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+mix
+  .browserSync({
+    proxy: 'localhost:8000',
+    open: true,
+  })
+  .js('resources/js/app.js', 'public/js')
+  .sass('resources/sass/app.scss', 'public/css')
+  .js('resources/js/admin/app.js', 'js/admin')
+  // .sass('resources/sass/app.scss', 'css')
+  .options({
+    autoprefixer: {
+      options: {
+        grid: 'autoplace',
+        overrideBrowserslist: [
+          'last 5 version',
+          'ie >= 9',
+          'iOS >= 8.1',
+          'Android >= 4.4',
+          '>= 2%',
+          'Firefox >= 20',
+          'safari >= 8',
+        ],
+      },
+    },
+    postCss: [require('autoprefixer')()],
+  })
+  .sourceMaps(!mix.inProduction())
+  .webpackConfig({
+    devtool: mix.inProduction() ? 'none' : 'source-map',
+    plugins: [new VuetifyLoaderPlugin()],
+  })
+  .version();
